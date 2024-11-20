78ce65a39affbc20d72b0f2e77445a5d08bcd179
ozky23 <ozky23@gmail.com>
2024-11-20 20:25:39 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button, ButtonGroup } from "@mui/material";
+
+function App() {
+	return (
+		<>
+			<ButtonGroup variant="contained" aria-label="Basic button group">
+				<Button>One</Button>
+				<Button>Two</Button>
+				<Button>Three</Button>
+			</ButtonGroup>
+		</>
+	);
+}
+
+export default App;
